2f99921a4d3dfb281d06d00cd1092e7577e21cbe
lab04 <lab04@lab4-16>
2024-08-19 22:47:55 -0300

    Tela de login com novos códigos.

--- LojaProdutos/src/br/com/etec/model/Operacoes.java
+++ LojaProdutos/src/br/com/etec/model/Operacoes.java
@@ -1,5 +1,48 @@
 package br.com.etec.model;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class Operacoes {
 	
+	@FXML
+	private TextField txfUsuario;
+	
+	@FXML
+	private PasswordField psfUsuario;
+	
+	@FXML
+	private Button btnAcessar;
+	
+	@FXML
+	private void acessarConta(ActionEvent event) {
+		
+		String nomeUsuario;
+		nomeUsuario = txfUsuario.getText();
+		
+		String senhaUsuario;
+		senhaUsuario = psfUsuario.getText();
+		
+		if (nomeUsuario.isEmpty() || senhaUsuario.isEmpty()) {
+			mostrarMensagem(Alert.AlertType.WARNING,"Faltando dados"," Sem usuario ou senha");
+			
+		}// if 
+		else {
+			mostrarMensagem(Alert.AlertType.WARNING,"Faltando dados","Informar a senha.");
+		}
+	}// acessarConta
+	
+	//---------------------------
+	
+	private void mostrarMensagem(Alert.AlertType tipo, String titulo, String mensagem ) {
+		Alert alerta = new Alert(tipo);
+		 alerta.setTitle(titulo);
+		 alerta.setHeaderText(null);
+		 alerta.setContentText(mensagem);
+		 alerta.showAndWait();
+	}
 }
--- LojaProdutos/src/module-info.java
+++ LojaProdutos/src/module-info.java
@@ -1,6 +1,8 @@
 module LojaProdutos {
 	requires javafx.controls;
 	requires javafx.fxml;
+	requires java.desktop;
+	requires javafx.base;
 	
 	opens br.com.etec.model to javafx.graphics, javafx.fxml;
 }
