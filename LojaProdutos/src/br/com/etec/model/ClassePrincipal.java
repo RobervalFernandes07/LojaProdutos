34f363f7d59c29201bf00a8e9f6aeead5d8ba5a9
lab04 <lab04@lab4-16>
2024-10-21 22:47:16 -0300

    Insert na tela principal

--- LojaProdutos/src/br/com/etec/model/ClassePrincipal.java
+++ LojaProdutos/src/br/com/etec/model/ClassePrincipal.java
@@ -1,6 +1,9 @@
 package br.com.etec.model;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 import javafx.collections.FXCollections;
@@ -44,5 +47,34 @@ public class ClassePrincipal implements Initializable {
 		
 		
 	}
+	
+	// Método para inserir dados no banco de dados
+    public void inserirDados(String valor) {
+        String sql = "INSERT INTO produto (tipo, marca, modelo) VALUES (?, ?, ?)";
+        
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        
+        try {
+            conn = Conexao.conectar();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, tipoProduto);
+            pstmt.setString(2, marcaProduto);
+            pstmt.setString(3, modeloProduto);
+            pstmt.executeUpdate();
+            System.out.println("Inserção bem-sucedida!");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+        	Conexao.fechar(conn);
+            if (pstmt != null) {
+                try {
+                    pstmt.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 
 }
