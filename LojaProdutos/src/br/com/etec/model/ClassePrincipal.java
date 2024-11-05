1dfdc90bb7467744bc547fbf9bb210c4df22a602
lab04 <lab04@lab4-17>
2024-11-04 22:24:57 -0300

    Método ComboBox

--- LojaProdutos/src/br/com/etec/model/ClassePrincipal.java
+++ LojaProdutos/src/br/com/etec/model/ClassePrincipal.java
@@ -3,6 +3,7 @@ package br.com.etec.model;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 
@@ -35,46 +36,71 @@ public class ClassePrincipal implements Initializable {
 	
 	public void initialize(URL url, ResourceBundle rb) {
 		
-		ObservableList<String> lista = FXCollections.observableArrayList("Chinelo" , "Tênis");
-		cmbTipo.setItems(lista);
+//		ObservableList<String> lista = FXCollections.observableArrayList("Chinelo" , "Tênis");
+//		cmbTipo.setItems(lista);
+//		
+//		ObservableList<String> lista2 = FXCollections.observableArrayList("Nike" , "Adidas");
+//		cmbTipo.setItems(lista2);
+//		
+//		ObservableList<String> lista3 = FXCollections.observableArrayList("TEN001" ,"CHN" ,"TEA002" , "CHA001");
+//		cmbTipo.setItems(lista3);
 		
-		ObservableList<String> lista2 = FXCollections.observableArrayList("Nike" , "Adidas");
-		cmbTipo.setItems(lista2);
+		// Chamar o metodo com as informações 
 		
-		ObservableList<String> lista3 = FXCollections.observableArrayList("TEN001" ,"CHN" ,"TEA002" , "CHA001");
-		cmbTipo.setItems(lista3);
-		
+		carregarComboBox(cmbTipo,"tipo");
+		carregarComboBox(cmbMarca,"marca");
+		carregarComboBox(cmbModelo,"modelo");
 		
 		
 	}
 	
 	// Método para inserir dados no banco de dados
-    public void inserirDados(String valor) {
-        String sql = "INSERT INTO produto (tipo, marca, modelo) VALUES (?, ?, ?)";
-        
-        Connection conn = null;
-        PreparedStatement pstmt = null;
-        
-        try {
-            conn = Conexao.conectar();
-            pstmt = conn.prepareStatement(sql);
-            pstmt.setString(1, tipoProduto);
-            pstmt.setString(2, marcaProduto);
-            pstmt.setString(3, modeloProduto);
-            pstmt.executeUpdate();
-            System.out.println("Inserção bem-sucedida!");
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        } finally {
-        	Conexao.fechar(conn);
-            if (pstmt != null) {
-                try {
-                    pstmt.close();
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
-
+//    public void inserirDados(String valor) {
+//        String sql = "INSERT INTO produto (tipo, marca, modelo) VALUES (?, ?, ?)";
+//        
+//        Connection conn = null;
+//        PreparedStatement pstmt = null;
+//        
+//        try {
+//            conn = Conexao.conectar();
+//            pstmt = conn.prepareStatement(sql);
+//            pstmt.setString(1, tipoProduto);
+//            pstmt.setString(2, marcaProduto);
+//            pstmt.setString(3, modeloProduto);
+//            pstmt.executeUpdate();
+//            System.out.println("Inserção bem-sucedida!");
+//        } catch (SQLException e) {
+//            System.out.println(e.getMessage());
+//        } finally {
+//        	Conexao.fechar(conn);
+//            if (pstmt != null) {
+//                try {
+//                    pstmt.close();
+//                } catch (SQLException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        }
+//    }
+		private void carregarComboBox(ComboBox<String> comBox, String coluna){
+			// Variavel lista = collections (guardar várias informações)
+			ObservableList<String> lista = FXCollections.observableArrayList();
+			
+			try (Connection conn = Conexao.conectar()){
+			// Pra executar o comando SQL e guardar na variável 
+			String sql = "SELECT DISTINCT" + coluna + "FROM minhatabela";
+			PreparedStatement stmt = conn.prepareStatement(sql);
+			ResultSet rs = stmt.executeQuery(sql);
+			
+			while(rs.next()) {
+				lista.add(rs.getString(coluna));
+			}
+			comBox.setItems(lista);
+			
+		}// try 
+			
+			catch(Exception e ) {
+				e.printStackTrace();
+			}
+		}
 }
